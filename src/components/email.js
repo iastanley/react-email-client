b07ab3c3a69168362df62abfe26ba97df1936a6a
iastanley <iasvks.craigslist@gmail.com>
2017-05-09 09:47:53 -0700

    found a fix for the redirect issue

--- src/components/email.js
+++ src/components/email.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+// import {createBrowserHistory} from 'history/createBrowserHistory';
 
 import Sidebar from './sidebar';
 import SingleEmail from './single-email';
 import EmailList from './email-list';
 import './email.css';
 
-export default function Email() {
+// const history = createBrowserHistory();
+
+export default function Email(props) {
+    if (window.location.pathname === "/") {
+      window.location = "/inbox";
+    }
     return (
       <Router>
         <div className="email">
             <Sidebar />
             <main>
-              <Redirect from="/" to="/inbox" />
               <Route exact path="/:folderId" component={EmailList}/>
               <Route exact path="/:folderId/:emailId" component={SingleEmail}/>
+
             </main>
         </div>
       </Router>
